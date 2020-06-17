5e534b6114d819aa623596a1b0ddd63a786368c5
Mikael1006 <mikael.moreau1006@gmail.com>
2020-06-17 13:16:19 +0200

    show sync in show info

--- src/app/features/explore/components/info-show/info-show.component.ts
+++ src/app/features/explore/components/info-show/info-show.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { ShowStoreService } from 'src/app/services/show-store.service';
 import { Show } from 'src/app/shared/models/show';
 
@@ -8,15 +8,25 @@ import { Show } from 'src/app/shared/models/show';
   templateUrl: './info-show.component.html',
   styleUrls: ['./info-show.component.scss']
 })
-export class InfoShowComponent implements OnInit {
+export class InfoShowComponent implements OnInit, OnDestroy {
 
-  show: Observable<Show>;
+  show: Show;
+  private subscriptions: Subscription;
 
   constructor(
     private showStoreService: ShowStoreService
-    ) {}
+    ) {
+      this.subscriptions = new Subscription();
+  }
 
   ngOnInit(): void {
-    this.show = this.showStoreService.getCurrentShow();
+    const subscription = this.showStoreService.getCurrentShow().subscribe(show => {
+      this.show = show;
+    });
+    this.subscriptions.add(subscription);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
